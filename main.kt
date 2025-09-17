d23187a6a1999f760a7274ef721af355c5d961b5
Dev Karan Singh <dev1604karan@gmail.com>
2025-09-17 23:52:47 +0530

    Add main function with variable declarations and print statements for numeric limits

---
+++ main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    var number: Long = 34;
+    val maxIntegerValue: Int = Int.MAX_VALUE;
+    val minIntegerValue: Int = Int.MIN_VALUE;
+    val maxLongValue: Long = Long.MAX_VALUE;
+    val minLongValue: Long = Long.MIN_VALUE;
+    val maxByteValue: Byte = Byte.MAX_VALUE;
+    val minByteValue: Byte = Byte.MIN_VALUE
+    val maxShortValue: Short = Short.MAX_VALUE;
+    val minShortValue: Short = Short.MIN_VALUE;
+    // val myNumber = 28l
+    val yourNumber = 28L
+
+
+    println("Hello DK");
+    println(maxIntegerValue);
+    println(minIntegerValue);
+    println(minLongValue);
+    println(maxLongValue);
+    println(maxByteValue);
+    println(minByteValue);
+    println(maxShortValue);
+    println(minShortValue)
+}
+
+    
